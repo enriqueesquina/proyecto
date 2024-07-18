5ce6605d24515f76cb1d2d30c38d7232f1fc5e59
EnriqueEsquia1212 <EnriqueEsquia1212@outlook.es>
2024-07-17 21:29:41 -0600

    2entidad

---
+++ proyec1/EN/Class2.cs
@@ -0,0 +1,11 @@
+﻿namespace proyec1.EN
+{
+    public class Class2
+    {
+        public int id {  get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public string description { get; set; }
+
+    }
+}
